6600eea802bf83a340d9d9bcec84e94df3e93a0f
tmcgroarty <timothysmcgroarty@gmail.com>
2025-12-03 13:03:59 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,41 +1,28 @@
 variable "region" {
-  description = "AWS region"
-  type        = string
-}
-
-variable "subnet_id" {
-  description = "Subnet where EC2 will be launched"
-  type        = string
-}
-
-variable "security_group_id" {
-  description = "Security group for the EC2 instance"
-  type        = string
+  type    = string
+  default = "us-east-2"
 }
 
 variable "key_name" {
-  description = "EC2 key pair name"
-  type        = string
+  type = string
 }
 
-variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
-}
-
-variable "instance_name" {
-  description = "Tag Name for the instance"
-  type        = string
-  default     = "terraform-steelhead"
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "m5.large"
+variable "security_group_ids" {
+  type = list(string)
 }
 
 variable "iam_instance_profile" {
-  description = "IAM instance profile name attached to EC2"
-  type        = string
+  type = string
+}
+
+variable "instances" {
+  description = "EC2 instances to create"
+  type = map(object({
+    ami                         = string
+    subnet_id                   = string
+    instance_type               = string
+    associate_public_ip_address = bool
+    ebs_size                    = number   # use 0 if not required
+    ebs_device_name             = string   # empty string if not required
+  }))
 }
